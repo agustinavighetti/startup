d503191943c255d3037bddac120441e86ae23334
Agustina Vighetti <agustinavighetti@yahoo.com.ar>
2016-09-22 19:16:25 -0300

    [Agustina Vighetti] - Exercise 4 and 6 - Corrected

--- 01-JS-DOM-APIs/script_4.js
+++ 01-JS-DOM-APIs/script_4.js
@@ -1,11 +1,17 @@
 'use: strict'
 
-    function FadeIn() {
-        var pText = document.createTextNode("Hello world");
-        var newp = document.createElement("p");
-        newp.appendChild(pText);
-        newp.id = 'hid';
-        var existingp = document.getElementById("hid");
-        var father = existingp.parentNode;
-        var newSon = father.replaceChild(newp, existingp);
-    }
+function FadeIn() {
+    var pText;
+    var newp;
+    var existingp;
+    var father;
+    var newSon;
+    
+    pText = document.createTextNode('Hello world');
+    newp = document.createElement('p');
+    newp.appendChild(pText);
+    newp.id = 'hid';
+    existingp = document.getElementById('hid');
+    father = existingp.parentNode;
+    newSon = father.replaceChild(newp, existingp);
+}
--- 01-JS-DOM-APIs/script_6.js
+++ 01-JS-DOM-APIs/script_6.js
@@ -1,23 +1,31 @@
 'use: strict'
 
-        function display() {
-            var xhttp = new XMLHttpRequest();
-            
-            xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                let obj_json, joke, newp, cont, oldp;
-                obj_json = JSON.parse(this.responseText);
-                joke = obj_json.value.joke;
-                newp = document.createElement("p");
-                cont = document.createTextNode(obj_json.value.joke);
-                newp.appendChild(cont);
-                newp.setAttribute("id","oldp");
-                oldp = document.getElementById("oldp");
-                document.getElementById("box").replaceChild(newp, oldp);
-            }
-            };
+function display() {
+    let xhttp;
+    let obj_json;
+    let joke;
+    let newp;
+    let cont;
+    let oldp;
 
-            xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+    xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
 
-            xhttp.send();
+        if (this.readyState == 4 && this.status == 200) {
+            obj_json = JSON.parse(this.responseText);
+            joke = obj_json.value.joke;
+            newp = document.createElement('p');
+            cont = document.createTextNode(obj_json.value.joke);
+            newp.appendChild(cont);
+            newp.id = 'oldp';
+            oldp = document.getElementById('oldp');
+
+            document.getElementById('box').replaceChild(newp, oldp);
         }
+    };
+
+    xhttp.open('GET', "http://api.icndb.com/jokes/random", true);
+
+    xhttp.send();
+}
