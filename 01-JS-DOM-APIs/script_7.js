a2425a437f10508433ea0767cd99440686cdcd1b
Agustina Vighetti <agustinavighetti@yahoo.com.ar>
2016-09-22 20:18:56 -0300

    [Agustina Vighetti] - Topic 1 - Exercise 7 - Fixed

--- 01-JS-DOM-APIs/script_7.js
+++ 01-JS-DOM-APIs/script_7.js
@@ -1,32 +1,40 @@
 'use: strict'
 
-    function display(method) {
-                var xhttp, url;
-                let myPromise;
-                url = 'http://api.icndb.com/jokes/random';
-                myPromise = new Promise(function (resolve, reject) {     
-                    xhttp = new XMLHttpRequest();
-                
-                    xhttp.open(method, url, true);
+function display(method) {
+    let xhttp;
+    let myPromise;
+    let obj_json;
+    let joke
+    let newp;
+    let cont
+    let oldp;
+    const url = 'http://api.icndb.com/jokes/random';
 
-                    xhttp.onload = function() {
-                        let obj_json, joke, newp, cont, oldp;
-                        obj_json = JSON.parse(this.responseText);
-                        joke = obj_json.value.joke;
-                        newp = document.createElement("p");
-                        cont = document.createTextNode(obj_json.value.joke);
-                        newp.appendChild(cont);
-                        newp.setAttribute("id","oldp");
-                        oldp = document.getElementById("oldp");
-                        document.getElementById("box").replaceChild(newp, oldp);
-                        resolve(alert('Here comes the joke!!'));
-                    };
+    myPromise = new Promise(function (resolve, reject) {     
+        xhttp = new XMLHttpRequest();
+    
+        xhttp.open(method, url, true);
 
-                    xhttp.onerror = function() {
-                        reject(alert('Ups! Something went wrong :('));                
-                    };
-                    
-                    xhttp.send();
-                    
-                });
-            }
+        xhttp.onload = function() {
+
+            obj_json = JSON.parse(this.responseText);
+            joke = obj_json.value.joke;
+            newp = document.createElement('p');
+            cont = document.createTextNode(obj_json.value.joke);
+            newp.appendChild(cont);
+            newp.id = 'oldp';
+
+            oldp = document.getElementById('oldp');
+
+            document.getElementById('box').replaceChild(newp, oldp);
+
+            resolve(alert('Here comes the joke!!'));
+        };
+
+        xhttp.onerror = function() {
+            reject(alert('Ups! Something went wrong :('));                
+        };
+        
+        xhttp.send();
+    });
+}
